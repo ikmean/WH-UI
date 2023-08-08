c714fa8f6fb1863cb52f95692c7e862acef5906c
Kakha Chankseliani <kakhachankseliani@gmail.com>
2023-08-08 10:06:03 +0400

    style(ButtonStyles): changes hover styles

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -4,13 +4,13 @@ import Button from "../button/Button";
 import { ReactComponent as GraySearch } from "../../lib/icons/graySearch.svg";
 
 export default function Search() {
-  return (
-    <SearchBox>
-      <div>
-        <GraySearch />
-        <span>Search for properties</span>
-      </div>
-      <Button text="Search" color="red"/>
-    </SearchBox>
-  );
+	return (
+		<SearchBox>
+			<div>
+				<GraySearch />
+				<span>Search for properties</span>
+			</div>
+			<Button text="Search" color="red" />
+		</SearchBox>
+	);
 }
