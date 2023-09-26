1d276fb7ba457980831153431853f76006540b50
Kakha Chankseliani <kakhachankseliani@gmail.com>
2023-09-26 13:59:19 +0400

    chore: last commit before leaving project

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { SearchBox } from "./SearchStyles";
 import Button from "../button/Button";
 import { ReactComponent as GraySearch } from "../../lib/icons/graySearch.svg";
+import { InputComponent } from "../input/Input";
 
 export default function Search() {
 	return (
 		<SearchBox>
 			<div>
 				<GraySearch />
-				<span>Search for properties</span>
+				<InputComponent placeholder="Search for properties" />
+				{/* <input
+					type="text"
+					name="search-properties"
+					placeholder="Search for properties"
+				/> */}
 			</div>
 			<Button text="Search" color="red" />
 		</SearchBox>
