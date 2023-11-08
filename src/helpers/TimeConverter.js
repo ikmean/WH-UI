0a5cdf8e13c8a551712486c71874d93cd8a9fa49
ikamean <ika.bidizshvili@gmail.com>
2023-11-08 21:26:26 +0400

    husky - prettier mix

--- src/helpers/TimeConverter.js
+++ src/helpers/TimeConverter.js
@@ -1,19 +1,17 @@
 export function getDate(date) {
-    const newDate = new Date(date)
-    const year = new Date(newDate).getFullYear()
-    const month = new Date(newDate).getMonth()
-    const day = new Date(newDate).getDate()
+  const newDate = new Date(date)
+  const year = new Date(newDate).getFullYear()
+  const month = new Date(newDate).getMonth()
+  const day = new Date(newDate).getDate()
 
-  const monthName =   getMonthName(month)
+  const monthName = getMonthName(month)
 
-    return `${monthName} ${day}, ${year}`
+  return `${monthName} ${day}, ${year}`
+}
 
-  }
+function getMonthName(monthNumber) {
+  const date = new Date()
+  date.setMonth(monthNumber - 1)
 
-
-  function getMonthName(monthNumber) {
-    const date = new Date()
-    date.setMonth(monthNumber - 1)
-
-    return date.toLocaleString("en-US", { month: "long" })
-  }
+  return date.toLocaleString('en-US', { month: 'long' })
+}
