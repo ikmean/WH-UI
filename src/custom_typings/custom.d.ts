4f36132b531307c18e815bf5ad3957ce7e1bbad1
ikmean <122292983+ikmean@users.noreply.github.com>
2023-11-08 21:28:00 +0400

    Husky (#12)
    * added husky
    
    * husky - prettier mix
    
    ---------
    
    Co-authored-by: ikamean <ika.bidizshvili@gmail.com>

--- src/custom_typings/custom.d.ts
+++ src/custom_typings/custom.d.ts
@@ -1,17 +1,17 @@
-declare module "*.mp3";
-declare module "*.mp4";
-declare module "*.webm";
-declare module "*.mov";
+declare module '*.mp3'
+declare module '*.mp4'
+declare module '*.webm'
+declare module '*.mov'
 
-declare module "*.svg";
+declare module '*.svg'
 
 // eslint-disable-next-line no-useless-escape
-declare module "*.svg" {
-  import React = require("react");
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
+declare module '*.svg' {
+  import React = require('react')
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
+  export default ReactComponent
 }
 
-declare module "*.png" {
-  export default "" as string;
+declare module '*.png' {
+  export default '' as string
 }
