cd6164b666f5701700c396abcb9d50c8d602c879
ikamean <ika.bidizshvili@gmail.com>
2024-01-16 17:25:24 +0400

    removed intersection observer api and slowLoader

--- src/components/findHouse/FindHouse.tsx
+++ src/components/findHouse/FindHouse.tsx
@@ -3,32 +3,21 @@ import { useTranslation } from 'react-i18next'
 import { Wrapper, BoxWrapper } from './FindHouseStyles'
 import Box from './Box'
 import Button from '../button/Button'
-import { useInView } from 'react-intersection-observer'
-import { SlowLoader } from '../featured/FeaturedStyles'
 
 export default function FindHouse() {
   const { t } = useTranslation()
 
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-    rootMargin: '-300px'
-  })
-
   return (
-    <div ref={ref}>
-      {inView && (
-        <SlowLoader>
-          <Wrapper>
-            <h1>{t('FindHouseHeader')}</h1>
-            <BoxWrapper>
-              <Box icon='findHouse' title={t('FindHouseText1')} description={t('FindHouseDesc1')} />
-              <Box icon='calendar' title={t('FindHouseText2')} description={t('FindHouseDesc2')} />
-              <Box icon='house' title={t('FindHouseText3')} description={t('FindHouseDesc3')} />
-            </BoxWrapper>
-            <Button text={t('SearchForProperties')} color='black' to='/properties' />
-          </Wrapper>
-        </SlowLoader>
-      )}
+    <div>
+      <Wrapper>
+        <h1>{t('FindHouseHeader')}</h1>
+        <BoxWrapper>
+          <Box icon='findHouse' title={t('FindHouseText1')} description={t('FindHouseDesc1')} />
+          <Box icon='calendar' title={t('FindHouseText2')} description={t('FindHouseDesc2')} />
+          <Box icon='house' title={t('FindHouseText3')} description={t('FindHouseDesc3')} />
+        </BoxWrapper>
+        <Button text={t('SearchForProperties')} color='black' to='/properties' />
+      </Wrapper>
     </div>
   )
 }
--- src/components/properties/Properties.tsx
+++ src/components/properties/Properties.tsx
@@ -1,36 +1,26 @@
 import React from 'react'
 import FeatureHeader from '../featured/Header'
 import FeaturedBox from '../featured/FeturedBox'
-import { PropertiesWrapper, SlowLoader } from '../featured/FeaturedStyles'
-import { useInView } from 'react-intersection-observer'
+import { PropertiesWrapper } from '../featured/FeaturedStyles'
 
 export default function Properties({ title, properties }: any) {
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-    rootMargin: '-200px'
-  })
-
   return (
-    <div ref={ref}>
-      {inView && (
-        <SlowLoader>
-          <FeatureHeader title={title} />
-          {properties && (
-            <PropertiesWrapper>
-              {properties.map((data: any, i: number) => (
-                <FeaturedBox
-                  data={{
-                    ...data,
-                    featured: false,
-                    main: false,
-                    id: data.id
-                  }}
-                  key={i}
-                />
-              ))}
-            </PropertiesWrapper>
-          )}
-        </SlowLoader>
+    <div>
+      <FeatureHeader title={title} />
+      {properties && (
+        <PropertiesWrapper>
+          {properties.map((data: any, i: number) => (
+            <FeaturedBox
+              data={{
+                ...data,
+                featured: false,
+                main: false,
+                id: data.id
+              }}
+              key={i}
+            />
+          ))}
+        </PropertiesWrapper>
       )}
     </div>
   )
