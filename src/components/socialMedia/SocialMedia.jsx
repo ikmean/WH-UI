49f78b5d5d9c014685575d9def317f2fb422b1fb
ikmean <122292983+ikmean@users.noreply.github.com>
2024-04-12 20:50:48 +0400

    Agent box (#71)
    * Agents Page Layout
    
    * changed agents into team naming
    
    * social media icons for footer
    
    ---------
    
    Co-authored-by: ikamean <ika.bidizshvili@gmail.com>

--- src/components/socialMedia/SocialMedia.jsx
+++ src/components/socialMedia/SocialMedia.jsx
@@ -1,37 +1,38 @@
 import React from 'react'
 import { SocialMediaWrapper, Social } from './SocialMediaStyles'
 import { ReactComponent as Facebook } from '../../lib/icons/facebook.svg'
-import { ReactComponent as Twitter } from '../../lib/icons/Twitter.svg'
+import { ReactComponent as Instagram } from '../../lib/icons/instagram.svg'
 import { ReactComponent as Linkedin } from '../../lib/icons/linkedin.svg'
-import { ReactComponent as Youtube } from '../../lib/icons/youtube.svg'
+import { ReactComponent as TikTok } from '../../lib/icons/tiktok.svg'
+
 import { ReactComponent as Whatsapp } from '../../lib/icons/whatsapp.svg'
 
 export default function SocialMedia() {
   return (
     <SocialMediaWrapper>
       <Social>
-        <a href='https://www.facebook.com/' rel='noreferrer' target='_blank'>
+        <a href='https://www.facebook.com/YourWarmHouse' rel='noreferrer' target='_blank'>
           <Facebook />
         </a>
       </Social>
       <Social>
-        <a href='https://www.facebook.com/' rel='noreferrer' target='_blank'>
-          <Twitter />
-        </a>
-      </Social>
-      <Social>
-        <a href='https://www.facebook.com/' rel='noreferrer' target='_blank'>
+        <a href='https://www.linkedin.com/company/warm-house-თბილი-სახლი' rel='noreferrer' target='_blank'>
           <Linkedin />
         </a>
       </Social>
       <Social>
         <a href='https://www.facebook.com/' rel='noreferrer' target='_blank'>
-          <Youtube />
+          <Whatsapp />
         </a>
       </Social>
       <Social>
-        <a href='https://www.facebook.com/' rel='noreferrer' target='_blank'>
-          <Whatsapp />
+        <a href='https://www.instagram.com/warm.ge' rel='noreferrer' target='_blank'>
+          <Instagram />
+        </a>
+      </Social>
+      <Social>
+        <a href='https://www.tiktok.com/@warm.ge' rel='noreferrer' target='_blank'>
+          <TikTok />
         </a>
       </Social>
     </SocialMediaWrapper>
